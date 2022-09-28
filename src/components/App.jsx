dbab590f3f99a3372b72de53685439106e6852e8
Bykova Kateryna <bykova.kateryna@gmail.com>
2022-09-28 10:23:53 +0300

    HW4 Phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { GlobalStyle } from './GlobalStyle';
+import  {PhoneBoockContainer}  from './Phonebook/Phonebook.styled';
+import  PhoneBookSection  from './Phonebook/Phonebook'
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <PhoneBoockContainer>
+    <GlobalStyle />
+    <PhoneBookSection />
+  </PhoneBoockContainer>
   );
 };
